ca0604432a8145ba5df69d69f162b50ec3620bf3
TH <mautilusai.project@gmail.com>
2022-06-26 12:41:07 +0200

    app ergänzt

--- streamlit.py
+++ streamlit.py
@@ -3,6 +3,23 @@ import os
 
 import streamlit as st
 
+import tensorflow as tf
+
+import numpy as np
+
+def load_model(path_to_dir):
+    reloaded = tf.keras.models.load_model(path_to_dir)
+    return reloaded
+
+def inference(image, model):
+    img = tf.image.resize(image, [224,224])
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+    predictions = model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+    return score
+
 if __name__ == '__main__':
     st.title('Welcome To Project Nautilus AI!')
     
@@ -20,6 +37,12 @@ if __name__ == '__main__':
         img = Image.open(file)
         st.title("Here is the image you've selected")
         st.image(img)
+
+        st.title("Inference Result")
+        model = load_model('inference_graph\\1654458544')
+        score = inference(img, model)
+        class_names = ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+        st.write("This image most likely belongs to {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score)], 100 * np.max(score)))
     else:
         pass
     
