63666fd828bc2d16066ecc893653e64ac3f97ff7
nautilusai-project <108093855+nautilusai-project@users.noreply.github.com>
2022-07-18 16:29:50 +0200

    changed place for normalization

--- streamlit.py
+++ streamlit.py
@@ -17,10 +17,10 @@ def load_model(path_to_dir):
 
 def inference(image, model):
     img = tf.image.resize(image, [224,224])
-    normalization_layer = tf.keras.layers.Rescaling(1./255)
-    img = normalization_layer(img)
     img_array = tf.keras.utils.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0) # Create a batch
+    normalization_layer = tf.keras.layers.Rescaling(1./255)
+    img_array = normalization_layer(img_array)
 
     predictions = model.predict(img_array)
     score = tf.nn.softmax(predictions[0])
